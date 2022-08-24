49c3de388016d506d3229340bcc33336e5ba86b1
VladPashkovsky <v.v.pashkovsky@gmail.com>
2022-08-24 21:16:39 +0300

    Form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from 'react-hook-form'
+import './App.css'
 
 function App() {
+  const {
+    register,
+    formState: { errors, isValid },
+    handleSubmit,
+    reset
+  } = useForm({mode: 'onChange'})
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data))
+    reset()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>React-Hook-Form</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label> First Name:
+          <input {...register('firstName',
+            {
+              required: 'Поле обязательно для заполнения',
+              minLength: { value: 5, message: 'Минимум 5 символов' },
+            })} />
+        </label>
+        <div style={{ height: 20 }}>
+          {errors?.firstName && <p>{errors?.firstName?.message || 'Error'}</p>}
+        </div>
+        <label> Last Name:
+          <input {...register('lastName',
+            {
+              required: 'Поле обязательно для заполнения',
+              minLength: { value: 5, message: 'Минимум 5 символов' },
+            })} />
+        </label>
+        <div style={{ height: 20 }}>
+          {errors?.lastName && <p>{errors?.lastName?.message || 'Error'}</p>}
+        </div>
+        <input type='submit' disabled={!isValid}/>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
